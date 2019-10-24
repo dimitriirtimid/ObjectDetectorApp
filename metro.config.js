825877fd20380aef88c0af02ca239285dcef3f9b
Dimitri Dekker <dimitri.dekker@nl.abnamro.com>
2019-10-24 23:22:44 +0200

    tensor flow native installed

---
+++ metro.config.js
@@ -0,0 +1,23 @@
+// Copied from https://github.com/tensorflow/tfjs/tree/master/tfjs-react-native#step-3-configure-metro 
+// and added 'ttf' to the assetExts
+
+// Change 1 (import the blacklist utility)
+const blacklist = require('metro-config/src/defaults/blacklist');
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    // Change 2 (add 'bin' to assetExts)
+    assetExts: ['bin', 'txt', 'jpg', 'ttf'],
+    sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx'],
+    // Change 3 (add platform_node to blacklist)
+    blacklistRE: blacklist([/platform_node/])
+  },
+};
