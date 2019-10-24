864e843a180cc54133982e32c5ecab1ce8ee2e86
Dimitri Dekker <dimitri.dekker@nl.abnamro.com>
2019-10-24 23:45:16 +0200

    pre react native canvas

--- App.js
+++ App.js
@@ -1,38 +1,182 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { Fragment } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
+import { createCanvas } from 'canvas';
+// import { Base64 } from 'js-base64';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+import bike from './images/bicycle_base64_bin';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+// var fs = require('fs');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    justifyContent: 'center',
   },
 });
+
+/* Array of bytes to base64 string decoding */
+
+function b64ToUint6 (nChr) {
+
+  return nChr > 64 && nChr < 91 ?
+      nChr - 65
+    : nChr > 96 && nChr < 123 ?
+      nChr - 71
+    : nChr > 47 && nChr < 58 ?
+      nChr + 4
+    : nChr === 43 ?
+      62
+    : nChr === 47 ?
+      63
+    :
+      0;
+
+}
+
+function base64DecToArr (sBase64, nBlockSize) {
+
+  var
+    sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ""), nInLen = sB64Enc.length,
+    nOutLen = nBlockSize ? Math.ceil((nInLen * 3 + 1 >>> 2) / nBlockSize) * nBlockSize : nInLen * 3 + 1 >>> 2, aBytes = new Uint8Array(nOutLen);
+
+  for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+    nMod4 = nInIdx & 3;
+    nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+    if (nMod4 === 3 || nInLen - nInIdx === 1) {
+      for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+        aBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+      }
+      nUint24 = 0;
+    }
+  }
+
+  return aBytes;
+}
+
+/* UTF-8 array to DOMString and vice versa */
+
+function UTF8ArrToStr (aBytes) {
+
+  var sView = "";
+
+  for (var nPart, nLen = aBytes.length, nIdx = 0; nIdx < nLen; nIdx++) {
+    nPart = aBytes[nIdx];
+    sView += String.fromCharCode(
+      nPart > 251 && nPart < 254 && nIdx + 5 < nLen ? /* six bytes */
+        /* (nPart - 252 << 30) may be not so safe in ECMAScript! So...: */
+        (nPart - 252) * 1073741824 + (aBytes[++nIdx] - 128 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+      : nPart > 247 && nPart < 252 && nIdx + 4 < nLen ? /* five bytes */
+        (nPart - 248 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+      : nPart > 239 && nPart < 248 && nIdx + 3 < nLen ? /* four bytes */
+        (nPart - 240 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+      : nPart > 223 && nPart < 240 && nIdx + 2 < nLen ? /* three bytes */
+        (nPart - 224 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+      : nPart > 191 && nPart < 224 && nIdx + 1 < nLen ? /* two bytes */
+        (nPart - 192 << 6) + aBytes[++nIdx] - 128
+      : /* nPart < 127 ? */ /* one byte */
+        nPart
+    );
+  }
+
+  return sView;
+
+}
+
+
+// /      /      7      /      A      N      g      A      /      w      D
+// 111111 111111 111011 111111 000000 001101 100000 000000 111111 110000 000011
+// 1111 1111 1111 1110 1111 1111 0000 0000 1101 1000 0000 0000 1111 1111 0000 0000 11
+// FF        FE        FF        00        D8        00        FF        00
+
+// const decode = str => {
+//   const binstr = Base64.atob(str);
+//   const arr = new Uint8Array(binstr.length);
+//   for (var i = 0; i < binstr.length; i++) {
+//     arr[i] = binstr.charCodeAt(i);
+//   }
+//   return arr;
+// }
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isTfReady: false,
+      message: 'initial',
+    }
+  }
+// export default function App() {
+  
+render() {
+
+var myArray = base64DecToArr("QmFzZSA2NCDigJQgTW96aWxsYSBEZXZlbG9wZXIgTmV0d29yaw==");
+  return (
+    <View style={styles.container}>
+      <Fragment>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{'my binary: ' + UTF8ArrToStr(myArray)}</Text>
+      <Text>{this.state.message}</Text>
+      <Button
+                    title="Analyse"
+                    onPress={this.mountModel} 
+                />
+      </Fragment>
+    </View>
+  );
+}
+
+getCanvas = async () => {
+  const arr = base64DecToArr(bike);
+  console.log('array:' + arr[0]);
+  const blob = new Blob([arr], {type : 'image/jpg'});
+  console.log('blob created');
+  const canvas = createCanvas(800, 600);
+  const ctx = canvas.getContext('2d');
+  console.log('canvas created');
+
+  const imageBitMap = await createImageBitmap(blob);
+  console.log('bitmap created');
+
+  ctx.drawImage(imageBitMap, 0, 0);
+  console.log('image drawn');
+  return canvas;
+}
+
+
+  mountModel = async () => {
+    // Wait for tf to be ready.
+    try{
+    console.log("starting tf");
+    await tf.ready();        
+    this.setState({  message: 'tf ready' });
+    // Signal to the app that tensorflow.js can now be used.
+    console.log("tf ready");
+    // const model = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+
+    const model = await cocoSsd.load();
+    this.setState({  message: 'model loaded' });
+    console.log("model loaded");
+
+    const canvas = await this.getCanvas();
+    console.log("canvas built");
+
+    const predictions = await model.detect(canvas);
+    this.setState({  message: 'success: ' + predictions });
+    console.log("success: ", predictions);
+
+    }
+    catch (err) {
+    this.setState({  message: 'error - ' + err });
+
+    }
+
+  }
+
+}
