ddf8ef22bd605c05d7e32192cc2cf7de49384625
Dimitri Dekker <dimitri.dekker@nl.abnamro.com>
2019-10-25 19:56:24 +0200

    object detection working on mobile

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import '@tensorflow/tfjs-react-native';
 import { createCanvas } from 'canvas';
 // import { Base64 } from 'js-base64';
 import Canvas from 'react-native-canvas';
+import jpeg from 'jpeg-js';
+
 
 import bike from './images/bicycle_base64_bin';
 
@@ -124,14 +126,14 @@ export default class App extends React.Component {
   }
 
 render() {
-  const img =        <Image source={require('./bicycle.jpeg')} 
-  style={{width: "10%", height: "10%"}}
-  onload={() => {
-    console.log('Image loaded!');
-    // ctx.drawImage(img, 0, 0);
+  // const img =        <Image source={require('./bicycle.jpeg')} 
+  // style={{width: "10%", height: "10%"}}
+  // onload={() => {
+  //   console.log('Image loaded!');
+  //   // ctx.drawImage(img, 0, 0);
 
-  }}
-  />
+  // }}
+  // />
 
 var myArray = base64DecToArr("QmFzZSA2NCDigJQgTW96aWxsYSBEZXZlbG9wZXIgTmV0d29yaw==");
   return (
@@ -142,50 +144,112 @@ var myArray = base64DecToArr("QmFzZSA2NCDigJQgTW96aWxsYSBEZXZlbG9wZXIgTmV0d29yaw
       <Text>{this.state.message}</Text>
       <Button
                     title="Analyse"
-                    onPress={ () => this.mountModel(img) } 
+                    onPress={ () => this.mountModel() } 
                 />
        <Canvas ref={this.handleCanvas}/>
-       {img}
+       {/* {img} */}
       </Fragment>
     </View>
   );
 }
 
-getCanvas = async (img) => {
+getCanvas = async () => {
   const arr = base64DecToArr(bike);
   console.log('array:' + arr[0]);
   const blob = new Blob([arr], {type : 'image/jpg'});
   console.log('blob created');
-  const canvas = createCanvas(800, 600);
-  const ctx = canvas.getContext('2d');
+  // const canvas = createCanvas(1000, 1000);
   
-  // const canvas = this.state.canvas;
-  // if (!canvas)
-  //   throw new Error("No canvas in state");
+  const canvas = this.state.canvas;
+  if (!canvas)
+    throw new Error("No canvas in state");
+
+  const ctx = canvas.getContext('2d');
   console.log('canvas created');
 
 
 
-      // img.defaultSource = require('./bicycle.jpeg');
-      // img.onload = () => {
-      //   console.log('Image loaded!');
-      //   // ctx.drawImage(img, 0, 0);
-      // };
 
-  const imageBitMap = await createImageBitmap(blob);
-  console.log('bitmap created', imageBitMap);
 
-  for (var prop in imageBitMap) {
-    console.log(prop + ': ' + imageBitMap[prop]);
-  }
+  // const imageBitMap = await createImageBitmap(blob);
+  // console.log('bitmap created', imageBitMap);
 
-  ctx.drawImage(imageBitMap, 0, 0);
+  // for (var prop in imageBitMap) {
+  //   console.log(prop + ': ' + imageBitMap[prop]);
+  // }
+
+  // ctx.drawImage(imageBitMap, 0, 0);
   console.log('image drawn', ctx);
+
+  console.log('decoding jpeg');
+  var rawImageData = jpeg.decode(arr);
+  console.log(rawImageData);
+
+  var myImageData = ctx.getImageData(0, 0, rawImageData.width, rawImageData.height);
+  // console.log('image data pre copy:', myImageData);
+
+  for (var i = 0; i < rawImageData.width * rawImageData.height * 4; i++)
+  myImageData.data[i] = rawImageData.data[i];
+    // console.log('image data post copy:', myImageData);
+
+    ctx.putImageData(myImageData, 0, 0);
+
+  // ctx.drawImage(rawImageData, 0, 0);
+
+
   return canvas;
 }
 
 
-  mountModel = async (img) => {
+getCanvas2 = async () => {
+  const arr = base64DecToArr(bike);
+  console.log('array:' + arr[0]);
+  // const blob = new Blob([arr], {type : 'image/jpg'});
+  // console.log('blob created');
+  
+  const canvas = this.state.canvas;
+  if (!canvas)
+    throw new Error("No canvas in state");
+
+  // const ctx = canvas.getContext('2d');
+  // console.log('canvas created');
+
+
+  console.log('decoding jpeg');
+  var rawImageData = jpeg.decode(arr);
+  // console.log(rawImageData);
+
+  // var myImageData = ctx.getImageData(0, 0, rawImageData.width, rawImageData.height);
+
+  // for (var i = 0; i < rawImageData.width * rawImageData.height * 4; i++)
+  // myImageData.data[i] = rawImageData.data[i];
+
+  //   ctx.putImageData(myImageData, 0, 0);
+
+  return canvas;
+}
+
+  imageToTensor() {
+    const rawImageData = base64DecToArr(bike);
+    console.log('array built');
+
+    const TO_UINT8ARRAY = true
+    const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY)
+    // Drop the alpha channel info for mobilenet
+    const buffer = new Uint8Array(width * height * 3)
+    let offset = 0 // offset into original data
+    for (let i = 0; i < buffer.length; i += 3) {
+      buffer[i] = data[offset]
+      buffer[i + 1] = data[offset + 1]
+      buffer[i + 2] = data[offset + 2]
+
+      offset += 4
+    }
+
+    return tf.tensor3d(buffer, [height, width, 3])
+  }
+
+  mountModel = async () => {
     // Wait for tf to be ready.
     try{
     console.log("starting tf");
@@ -194,15 +258,20 @@ getCanvas = async (img) => {
     // Signal to the app that tensorflow.js can now be used.
     console.log("tf ready");
     // const model = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+    // const model = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/savedmodel/ssdlite_mobilenet_v2/model.json');
 
     const model = await cocoSsd.load();
     this.setState({  message: 'model loaded' });
     console.log("model loaded");
 
-    const canvas = await this.getCanvas();
-    console.log("canvas built");
+    // const canvas = await this.getCanvas2();
+    // console.log("canvas built");
 
-    const predictions = await model.detect(canvas);
+    const tensor = this.imageToTensor();
+    console.log('tensor created');
+
+    // const predictions = await model.detect(canvas);
+    const predictions = await model.detect(tensor);
     this.setState({  message: 'success: ' + predictions });
     console.log("success: ", predictions);
 
